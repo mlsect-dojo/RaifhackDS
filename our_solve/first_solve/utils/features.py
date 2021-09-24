d74809a39ef81bb1fadc03b00106bbbb808e76c4
vaskers5 <danil.krokodil@mail.ru>
2021-09-25 02:08:50 +0300

    add floor processer

--- our_solve/first_solve/utils/features.py
+++ our_solve/first_solve/utils/features.py
@@ -3,6 +3,36 @@ from geopy.distance import lonlat, distance
 from typing import Tuple, List
 import numpy as np
 from tqdm import tqdm
+from typing import Optional
+import pandas as pd
+
+
+def process_floor(smt: object):
+    if pd.isna(smt):
+        return None
+    try:
+        return int(smt)
+    except(ValueError):
+        try:
+            numbers = [str(i) for i in range(10)]
+            result = []
+            index = 0
+            indexes = [smt.find(elem) for elem in numbers if smt.find(elem) != -1] + [99999]
+            l_min = min(indexes)
+            if l_min == 99999:
+                return None
+            else:
+                while True:
+                    try:
+                        result += [int(smt[l_min])]
+                        l_min += 1
+                    except(ValueError):
+                        break
+                    except(IndexError):
+                        break
+                return int("".join(str(elem) for elem in result))
+        except(ValueError):
+            return None
 
 
 def get_distances(place_coords: List[Tuple[float, float]],
