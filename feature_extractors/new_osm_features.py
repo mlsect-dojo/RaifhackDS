204b01f2a1889202cb3e198d18a3e901d039699e
Suslov Mikhail <misha.suslov.2016@mail.ru>
2021-09-25 21:12:33 +0300

    create osm_features

--- feature_extractors/new_osm_features.py
+++ feature_extractors/new_osm_features.py
@@ -21,4 +21,20 @@ def make_div(data: pd.DataFrame, feature1: str, feature2: str) -> pd.Series:
     return data[[feature1, feature2]].div(axis=1)
 
 
+def min_distance_to_transport(data: pd.DataFrame, feature1: str, feature2: str) -> pd.Series:
+    """
+        находим минимальный путь до ближайшего транспорта('osm_subway_closest_dist' and 'osm_transport_stop_closest_dist')
+    """
+    return data[[feature1, feature2]].min(axis=1)
 
+
+def min_distance_to_city(data: pd.DataFrame, feature1: str, feature2: str) -> pd.Series:
+    """
+    находим минимальный путь до ближайшего города('osm_city_closest_dist' + 'osm_transport_stop_closest_dist')
+    """
+    return data[[feature1, feature2]].sum(axis=1)
+
+
+FUNCTIONS_DICT = {'*': make_product, '+': make_sum,
+                  '-': make_sub, '/': make_div, 'min_dist': min_distance_to_transport,
+                  'nearest_path_city': min_distance_to_city}
