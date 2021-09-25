9d88e0b05af8afd45a837b68114a9b10f0e415e9
vaskers5 <danil.krokodil@mail.ru>
2021-09-25 15:42:04 +0300

    add floor preprocessor

--- solutions/feature_extractors/floor.py
+++ solutions/feature_extractors/floor.py
@@ -1,7 +1,15 @@
 from statistics import mean
 from typing import Optional
 import pandas as pd
+import numpy as np
 
+
+def floor_preprocess(df: pd.DataFrame) -> pd.DataFrame:
+    preprocess_floor = df['floor'].apply(parse_floor)
+    floor = 1.93
+    preprocess_floor = preprocess_floor.apply(lambda x: floor if pd.isna(x) else x)
+    df['floor'] = preprocess_floor.apply(lambda x: np.log1p(x) if x > 0 else x)
+    return df
 
 def parse_floor(floor: Optional[str]) -> Optional[int]:
 
